b4f58191e2af9fff997f2f80fcab03bb6763342a
kuhuhalder <kuhuhalder2701@gmail.com>
2022-01-11 00:32:26 -0500

    rendering list of items

--- app/src/main/java/com/example/todoapp/MainActivity.kt
+++ app/src/main/java/com/example/todoapp/MainActivity.kt
@@ -2,10 +2,25 @@ package com.example.todoapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 
 class MainActivity : AppCompatActivity() {
+    val listOfTasks = mutableListOf<String>()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+//       findViewById<Button>(R.id.button).setOnClickListener()
+//       {
+//
+//       }
+        // Look up recycler view in the layout
+        val recyclerView = findViewById<RecyclerView>(R.id.recyclerView)
+        val adapter = TaskItemAdapter(listOfTasks)
+        recyclerView.adapter = adapter
+        recyclerView.layoutManager = LinearLayoutManager(this)
+
     }
 }
---
+++ app/src/main/java/com/example/todoapp/TaskItemAdapter.kt
@@ -0,0 +1,44 @@
+package com.example.todoapp
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+
+/*
+a bridge that that tells the recycler view how to display the data
+*/
+
+class TaskItemAdapter(val listOfItems: List<String>) :
+    RecyclerView.Adapter<TaskItemAdapter.ViewHolder>() {
+
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        val context = parent.context
+        val inflater = LayoutInflater.from(context)
+        // Inflate the custom layout
+        val contactView = inflater.inflate(android.R.layout.simple_list_item_1, parent, false)
+        // Return a new holder instance
+        return ViewHolder(contactView)
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+
+        // get teh data model based on position
+        val item = listOfItems.get(position)
+        holder.textView.text = item
+    }
+
+    override fun getItemCount(): Int {
+        return listOfItems.size
+    }
+
+    inner class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+        //Store references to elements in our layout view
+        val textView: TextView
+        init{
+            textView = itemView.findViewById(android.R.id.text1)
+        }
+    }
+}
